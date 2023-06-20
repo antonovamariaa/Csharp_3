0bb13745c87ef13b7b99b8404b2cf751c0f9f6bf
maria <antonova.maria.a@gmail.com>
2023-06-20 20:53:21 +0300

    task2 task3 done

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] c1 = new int[3];
+int[] c2 = new int[3];
+
+System.Console.Write("input x1: ");
+c1[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("input y1: ");
+c1[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("input z1: ");
+c1[2] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("input x2: ");
+c2[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("input y2: ");
+c2[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("input z2: ");
+c2[2] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Round((Math.Sqrt(Math.Pow(c2[0] - c1[0], 2) + Math.Pow(c2[1] - c1[1], 2) + Math.Pow(c2[2] - c1[2], 2))), 2));
---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num > 0)
+{
+    int index = 1;
+    double[] array = new double[num];
+    while (index <= num)
+    {
+        array[index - 1] = Math.Pow(index, 3);
+        index++;
+    }
+    System.Console.WriteLine(string.Join(", ", array));
+
+}
+else { System.Console.WriteLine("error"); }
