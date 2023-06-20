982a0a4a4cd9139ef9ca8073359e97b95426f416
maria <antonova.maria.a@gmail.com>
2023-06-20 20:49:16 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("input number: ");
+string str = Console.ReadLine();
+
+if (str.Length == 5)
+{
+    if (str[0] == str[4] && str[1] == str[3]) { System.Console.WriteLine("yes"); }
+    else { System.Console.WriteLine("no"); }
+}
+else { System.Console.WriteLine("error"); }
